b7be765ba86741f7949f2701014cc6bce2565daa
Shah Nawaz <shuvo1824@hotmail.com>
2025-06-04 23:54:36 +0600

    chore: year 2024, day 1, part 1

--- challenges/2024/01/solution.rb
+++ challenges/2024/01/solution.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
+
 module Year2024
   class Day01 < Solution
     # @input is available if you need the raw data input
     # Call `data` to access either an array of the parsed data, or a single record for a 1-line input file
 
     def part_1
-      nil
+      distances = []
+      for i in 0..(data[0].length - 1) do
+        distances.append(data[0][i] - data[1][i])
+      end
+
+      distances.sum
     end
 
     def part_2
@@ -13,14 +19,25 @@ module Year2024
     end
 
     private
-      # Processes each line of the input file and stores the result in the dataset
-      # def process_input(line)
-      #   line.map(&:to_i)
-      # end
 
-      # Processes the dataset as a whole
-      # def process_dataset(set)
-      #   set
-      # end
+    # Processes each line of the input file and stores the result in the dataset
+    def process_input(line)
+      line.split(' ').map(&:to_i)
+    end
+
+    # Processes the dataset as a whole
+    def process_dataset(set)
+      res = []
+      l0 = []
+      l1 = []
+
+      set.each do |arr|
+        l0.append arr[0]
+        l1.append arr[1]
+      end
+
+      res.append l0.sort
+      res.append l1.sort
+    end
   end
 end
